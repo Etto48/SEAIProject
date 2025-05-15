67fab4feea470833818714fb8b942eb8318e6c47
Etto48 <ettore.ricci.coding@gmail.com>
2025-05-15 16:40:53 +0200

    yo mama jokes

--- gate.py
+++ gate.py
@@ -5,7 +5,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_device(device)
 
 class GateAutoencoder(nn.Module):
-    def __init__(self, in_out_shape=(3,32,32),  depth=3, ff_depth=3, hidden_dim=128, latent_dim=128):
+    def __init__(self, in_out_shape=(3,32,32),  depth=3, conv_block_depth=3, ff_depth=3, hidden_dim=128, latent_dim=128):
         super(GateAutoencoder, self).__init__()
         self.input_features = in_out_shape[0]
         self.depth = depth
@@ -15,15 +15,12 @@ class GateAutoencoder(nn.Module):
         self.encoder = nn.Sequential()
         self.decoder = nn.Sequential()
         latent_width, latent_height = in_out_shape[1] // (2**depth), in_out_shape[2] // (2**depth)
+        self.encoder.append(nn.Conv2d(self.input_features, hidden_dim, kernel_size=5, padding=2, padding_mode='reflect'))
         for i in range(depth):
-            if i == 0:
-                in_channels = self.input_features
-            else:
-                in_channels = hidden_dim
-            out_channels = hidden_dim
-            self.encoder.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, padding_mode='reflect'))
-            self.encoder.append(nn.BatchNorm2d(out_channels))
-            self.encoder.append(nn.ReLU())
+            for j in range(conv_block_depth):
+                self.encoder.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, padding=1, padding_mode='reflect'))
+                self.encoder.append(nn.BatchNorm2d(hidden_dim))
+                self.encoder.append(nn.ReLU())
             self.encoder.append(nn.MaxPool2d(kernel_size=2, stride=2))
         self.encoder.append(nn.Flatten())
         for i in range(ff_depth):
@@ -59,15 +56,12 @@ class GateAutoencoder(nn.Module):
         self.decoder.append(nn.Unflatten(1, (hidden_dim, latent_width, latent_height)))
 
         for i in range(depth):
-            out_channels = hidden_dim
-            in_channels = hidden_dim
-            self.decoder.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2))
-            self.decoder.append(nn.BatchNorm2d(out_channels))
-            self.decoder.append(nn.ReLU())
-            self.decoder.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, padding_mode='reflect'))
-            self.decoder.append(nn.BatchNorm2d(out_channels))
-            self.decoder.append(nn.ReLU())
-        self.decoder.append(nn.Conv2d(hidden_dim, self.input_features, kernel_size=3, padding=1, padding_mode='reflect'))
+            self.decoder.append(nn.ConvTranspose2d(hidden_dim, hidden_dim, kernel_size=2, stride=2))
+            for j in range(conv_block_depth):
+                self.decoder.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, padding=1, padding_mode='reflect'))
+                self.decoder.append(nn.BatchNorm2d(hidden_dim))
+                self.decoder.append(nn.ReLU())
+        self.decoder.append(nn.Conv2d(hidden_dim, self.input_features, kernel_size=5, padding=2, padding_mode='reflect'))
         self.decoder.append(nn.Sigmoid())
 
     def forward(self, x: torch.Tensor):
