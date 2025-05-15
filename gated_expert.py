dd1875643065073760ffa724a63c42b93117323b
Etto48 <ettore.ricci.coding@gmail.com>
2025-05-15 17:39:18 +0200

    new shiz

--- gated_expert.py
+++ gated_expert.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_device(device)
 
 class GatedExpert(nn.Module):
-    def __init__(self, in_out_shape=(3, 32, 32), classes=10, depth=3, conv_block_depth=5, ff_depth=3, hidden_dim=128, latent_dim=128, task_aware=True):
+    def __init__(self, in_out_shape=(3, 32, 32), classes=10, depth=3, ff_depth=1, expert_depth=3, hidden_dim=128, latent_dim=128, task_aware=True):
         super(GatedExpert, self).__init__()
         self.gates = nn.ModuleList()
         self.experts = nn.ModuleList()
@@ -26,8 +26,8 @@ class GatedExpert(nn.Module):
         self.in_out_shape = in_out_shape
         self.classes = classes
         self.depth = depth
-        self.conv_block_depth = conv_block_depth
         self.ff_depth = ff_depth
+        self.expert_depth = expert_depth
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
         self.hold_time_after_new_task = 10
@@ -44,7 +44,6 @@ class GatedExpert(nn.Module):
         gate = GateAutoencoder(
             in_out_shape=self.in_out_shape,
             depth=self.depth,
-            conv_block_depth=self.conv_block_depth,
             ff_depth=self.ff_depth,
             hidden_dim=self.hidden_dim,
             latent_dim=self.latent_dim
@@ -52,7 +51,7 @@ class GatedExpert(nn.Module):
         self.gates.append(gate)
         expert = ExpertMLP(
             input_feature=self.latent_dim,
-            depth=self.ff_depth,
+            depth=self.expert_depth,
             hidden_features=self.hidden_dim,
             output_features=self.classes
         )
