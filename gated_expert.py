04634349e9781e79b7e3f4121bda3f77d44890e8
Etto48 <ettore.ricci.coding@gmail.com>
2025-05-15 18:21:44 +0200

    I surrender

--- gated_expert.py
+++ gated_expert.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_device(device)
 
 class GatedExpert(nn.Module):
-    def __init__(self, in_out_shape=(3, 32, 32), classes=10, depth=1, ff_depth=3, expert_depth=3, hidden_dim=64, latent_dim=1024, task_aware=True):
+    def __init__(self, in_out_shape=(1, 28, 28), classes=10, depth=2, ff_depth=3, expert_depth=3, hidden_dim=64, latent_dim=128, task_aware=True):
         super(GatedExpert, self).__init__()
         self.gates = nn.ModuleList()
         self.experts = nn.ModuleList()
@@ -194,9 +194,9 @@ class GatedExpert(nn.Module):
             plt.show()
 
 def main():
-    train_dataset = SplitCIFAR10(task_duration=10000)
+    train_dataset = SplitMNIST(task_duration=10000)
     #test_dataset = datasets.MNIST(root='data', train=False, download=True, transform=train_dataset.transform, target_transform=torch.tensor)
-    test_dataset = datasets.CIFAR10(root='data', train=False, download=True, transform=train_dataset.transform)
+    test_dataset = datasets.MNIST(root='data', train=False, download=True, transform=train_dataset.transform)
     model = GatedExpert()
     model.fit(train_dataset, test_dataset)
 
