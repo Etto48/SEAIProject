e58ae5aefca1ed1f5dc5a51d31d49f5a7922cbcc
Etto48 <ettore.ricci.coding@gmail.com>
2025-05-17 18:51:35 +0200

    ce but worse

--- lwf.py
+++ lwf.py
@@ -47,7 +47,7 @@ class LWFClassifier(nn.Module):
             classes)
         self.optimizer = torch.optim.SGD(self.parameters(), lr=1e-3)
         self.loss = nn.CrossEntropyLoss()
-        self.loss_old = nn.KLDivLoss(reduction='batchmean', log_target=True)
+        self.loss_old = lambda logx, logy: -torch.sum(torch.softmax(logy, dim=1) * torch.log_softmax(logx, dim=1), dim=1).mean()
 
         self.error_window_max_len = 32
         self.error_threshold = 10
